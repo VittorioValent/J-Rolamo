c9a0047eea6c103162895071e59bfa5f22f2f78b
VittorioValent <v.valent@contrader.it>
2020-01-08 13:11:37 +0100

    launch script updated

--- j-rolamo.sh
+++ j-rolamo.sh
@@ -7,13 +7,19 @@ echo -n "Project name: "
 
 read projectname
 
+echo "What is your project's packaging?"
+
+echo -n "Packaging: "
+
+read package
+
 echo -n "Do you want to run the project after installation? (y/N) " 
 
 read run
 
 echo "Creating a new Project with name "$projectname
 
-mvn archetype:generate -B -DarchetypeGroupId=it.contrader -DarchetypeArtifactId=j-rolamo -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=it.target -Dpackage=it.target.$projectname -DartifactId=$projectname -Dversion=1.0-SNAPSHOT -Dname=$projectname
+mvn archetype:generate -B -DarchetypeGroupId=it.contrader -DarchetypeArtifactId=j-rolamo -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=$package -Dpackage=$package.$projectname -DartifactId=$projectname -Dversion=1.0-SNAPSHOT -Dname=$projectname
 
 cd $projectname
 
